e1ee0ac791c8e8523c325103236f898c0fe4527d
Bao Nguyen <bao.nguyen@mobility-x.com>
2019-11-22 16:21:28 +0700

    Add extension and binding data to controller

--- BaseSource/Common/Extension/Reactive+Helper.swift
+++ BaseSource/Common/Extension/Reactive+Helper.swift
@@ -8,10 +8,17 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
+import MBProgressHUD
 
 extension Reactive where Base: UIViewController {
     public var viewDidLoad: Observable<Void> {
         return methodInvoked(#selector(UIViewController.viewDidLoad))
+            .map { _ in return }
+    }
+    
+    public var viewDidLayoutSubviews: Observable<Void> {
+        return methodInvoked(#selector(UIViewController.viewDidLayoutSubviews))
             .map { _ in return }
     }
     
@@ -23,5 +30,33 @@ extension Reactive where Base: UIViewController {
     public var viewDidAppear: Observable<Void> {
         return methodInvoked(#selector(UIViewController.viewDidAppear))
             .map { _ in return }
+    }
+    
+    public var viewWillDisappear: Observable<Void> {
+        return methodInvoked(#selector(UIViewController.viewWillDisappear))
+            .map { _ in return }
+    }
+    
+    public var viewDidDisappear: Observable<Void> {
+        return methodInvoked(#selector(UIViewController.viewDidDisappear))
+            .map { _ in return }
+    }
+    
+    public var showError: Binder<String> {
+        return Binder<String>.init(base.self, scheduler: MainScheduler.instance) { (controller, message) in
+            controller.showError(message: message)
+        }
+    }
+}
+
+extension Reactive where Base: UIView {
+    public var isShowHUD: Binder<Bool> {
+        return Binder<Bool>.init(base.self, scheduler: MainScheduler.instance) { (view, isLoading) in
+            if isLoading {
+                MBProgressHUD.showAdded(to: view, animated: true)
+            } else {
+                MBProgressHUD.hide(for: view, animated: true)
+            }
+        }
     }
 }
